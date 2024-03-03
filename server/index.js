2cbeaaff7fab0842a640dbc4d57b9f746b970c23
GabrielAlberini <77715611+GabrielAlberini@users.noreply.github.com>
2024-03-03 17:09:31 -0300

    add: socket.io configuration in server

--- server/index.js
+++ server/index.js
@@ -1,15 +1,25 @@
 import express from "express";
 import logger from "morgan";
 
+import { Server } from "socket.io";
+import { createServer } from "node:http";
+
 const PORT = process.env.PORT ?? 3000;
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
 app.use(logger("dev"));
 
 app.get("/", (req, res) => {
   res.sendFile(process.cwd() + "/client/index.html");
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
