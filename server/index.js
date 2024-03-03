8cae12a97db2c9db1e3029f4c25185d6aa50ab36
GabrielAlberini <77715611+GabrielAlberini@users.noreply.github.com>
2024-03-03 17:29:43 -0300

    add: styled client form chat

--- server/index.js
+++ server/index.js
@@ -12,6 +12,10 @@ const io = new Server(server);
 
 io.on("connection", (socket) => {
   console.log("a user connected");
+
+  socket.on("disconnect", () => {
+    console.log("an user has disconnected");
+  });
 });
 
 app.use(logger("dev"));
