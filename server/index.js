24f5044390de0669bd4082944d20844ecb59c1ad
GabrielAlberini <77715611+GabrielAlberini@users.noreply.github.com>
2024-03-03 18:03:09 -0300

    add: handling of messages from the client to the server and brodcast of the same

--- server/index.js
+++ server/index.js
@@ -13,6 +13,10 @@ const io = new Server(server);
 io.on("connection", (socket) => {
   console.log("a user connected");
 
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+
   socket.on("disconnect", () => {
     console.log("an user has disconnected");
   });
